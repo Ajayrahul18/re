d89ffb51024fa8f4994e0c6e466959dd2b3eb845
Ajayrahul18 <108588354+Ajayrahul18@users.noreply.github.com>
2022-12-15 08:35:58 +0530

    Second

--- Complete.py
+++ Complete.py
@@ -1,13 +1,30 @@
 class Contact:
-    def __add_contact__(self):
-        my_data = dict()
-        key = input("Enter a Name: ")
-        if key != 'q':
-         value1 = input("Enter a Phone: ")
-         value2 = input("Enter a Mali: ")
-         my_data[key] = value1,value2
-        return f'Contact has stored'
+    def __init__(self, name, phone, email):
+        self.name = name
+        self.phone = phone
+        self.email = email
+    def get_name(self):
+        return self.name
+    def get_phone(self):
+        return self.phone
+    def get_email(self):
+        return self.email
 
-contact = Contact()
-print(contact.__add_contact__())
-
+class ContactMap:
+    def __init__(self):
+        self.contacts = {}
+    def add_contact(self, contact):
+        self.contacts[contact.get_name()] = contact
+    def print_contact(self):
+        for name, contact in self.contacts.items():
+            print(f'Name: {contact.get_name()} Phone: {contact.get_phone()} Email: {contact.get_email()}')
+contact_map = ContactMap()
+while True:
+    name = input("Enter name (or 'q' to quit)")
+    if name == 'q':
+        break
+    phone = input('Enter the phone')
+    email = input('Enter Email')
+    contact = Contact(name, phone, email)
+    contact_map.add_contact(contact)
+contact_map.print_contact()
