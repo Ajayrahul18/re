3ee32339a335a085d0d4b141198a3068af639b52
Ajayrahul18 <108588354+Ajayrahul18@users.noreply.github.com>
2022-12-15 10:46:35 +0530

    Add files via upload

--- Complete.py
+++ Complete.py
@@ -15,16 +15,25 @@ class ContactMap:
         self.contacts = {}
     def add_contact(self, contact):
         self.contacts[contact.get_name()] = contact
+    def delete_contact(self, contact_name):
+        if contact_name in self.contacts:
+            del self.contacts[contact_name]
+    def find_contact(self, contact_name):
+        if contact_name in self.contacts:
+            print(self.contacts[contact_name])
     def print_contact(self):
         for name, contact in self.contacts.items():
             print(f'Name: {contact.get_name()} Phone: {contact.get_phone()} Email: {contact.get_email()}')
+            
 contact_map = ContactMap()
 while True:
-    name = input("Enter name (or 'q' to quit)")
+    name = input("Enter name (or 'q' to quit): ")
     if name == 'q':
         break
-    phone = input('Enter the phone')
-    email = input('Enter Email')
+    phone = input('Enter the phone: ')
+    email = input('Enter Email: ')
     contact = Contact(name, phone, email)
     contact_map.add_contact(contact)
-contact_map.print_contact()
+#contact_map.delete_contact('Rahul')
+contact_map.find_contact('Rahul')
+
